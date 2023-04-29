4e59672971d95d19193839716ba7ff3899be58e6
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-29 22:04:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Contact Me Form Email Notification
+
+This repository contains the code that helps to get the messages submitted through the contact me form of your website delivered to your email inbox.
+
+Feel free to contribute to this project if you want to add any new features or improvements.
+
+## Getting Started
+
+To get started with this project, follow these steps:
+
+1. Clone this repository to your local machine.
+2. npm i
+3. Embed the contact form code in your website.
+
+## Contributing
+
+If you wish to contribute to this project, feel free to create a pull request. I welcome any contributions that would help to improve this project.
+
+Please ensure that your code adheres to the coding standards and that any dependencies are properly documented.
---
+++ app.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+// Parse incoming request data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/contact-form-handler', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
+
+    // Create a nodemailer transporter object
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+
+    // Define the email options
+    const mailOptions = {
+        from: email,
+        to: 'abhinabroy2001@gmail.com',
+        subject: 'New message from your portfolio website',
+        text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('success');
+        }
+    });
+});
+
+export default app;
